0a7ef5985f163c1e6718858d854e64db511c90cb
Violetta Ryabtsev <wondervioletta@gmail.com>
2020-09-22 14:42:00 -0700

    added some changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import React from 'react'
 import { render } from 'react-dom'
-import pet from './pet';
+import Pet from './pet';
 
 
 // your code 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
+    // return React.createElement(
+    //     "div",
+    //     {},
 
-        [
-            React.createElement("h1", {}, "FirstApplication"),
-            React.createElement(pet, {
-                name: "luna",
-                animal: 'dog',
-                breed: 'buldog'
-            }, []),
+    //     [
+    //         React.createElement("h1", {}, "FirstApplication"),
+    //         React.createElement(pet, {
+    //             name: "luna",
+    //             animal: 'dog',
+    //             breed: 'buldog'
+    //         }, []),
 
-            React.createElement(pet, {
-                name: "Strudel",
-                animal: 'dog',
-                breed: 'pug'
-            }, []),
+    //         React.createElement(pet, {
+    //             name: "Strudel",
+    //             animal: 'dog',
+    //             breed: 'pug'
+    //         }, []),
 
-            React.createElement(pet, {
-                name: "Max",
-                animal: 'cat',
-                breed: 'fat cat '
-            }, [])
+    //         React.createElement(pet, {
+    //             name: "Max",
+    //             animal: 'cat',
+    //             breed: 'fat cat '
+    //         }, [])
 
-        ]
-    );
+    //     ]
+    // );
+    return (
+        <div>
+            <h1 id="something-important">'Adoped me'</h1>
+            < Pet name="Luna" animal="dog" breed="pug" />
+            < Pet name="Bob" animal="fish" breed="yellow" />
+            < Pet name="Roman" animal="cat" breed="fat" />
+        </div>
+    )
+
 };
 
-render(React.createElement(App),
-    document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
 
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,17 @@
 import React from 'react';
-export default function pet(props) {
-    return React.createElement('div', {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed)
-    ]);
+export default function Pet(props) {
+    // return React.createElement('div', {}, [
+    //     React.createElement("h1", {}, props.name),
+    //     React.createElement("h2", {}, props.animal),
+    //     React.createElement("h2", {}, props.breed)
+    // ]);
 
+    return (
+        <div>
+            <h1 id="colorName"> Name: {props.name.toUpperCase()}</h1>
+            <h2>{props.animal}</h2>
+            <h2>{props.breed}</h2>
+        </div>
+
+    );
 };
